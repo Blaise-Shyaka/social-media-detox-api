8d7dfeefe556c35a46edb54be222f2a5c5a1f81c
Blaise-Shyaka <blaiseshyaka95@gmail.com>
2021-07-28 19:10:45 +0200

    Add class to handle JWT encode and decode

---
+++ app/lib/json_web_token.rb
@@ -0,0 +1,25 @@
+# This class will be used to encode and decode users's passwords based on a secret key
+class JsonWebToken
+
+  # Each rails application has a unique ID. We will use it as our secret key
+  SECRET_KEY = Rails.application.secrets.secret_key_base
+
+  def self.encode(payload, exp = 24.hours.from_now)
+    # Set expiry date of the token
+    payload[:exp] = exp.to_i
+
+    # Sign token
+    JWT.encode(payload, SECRET_KEY)
+  end
+
+  def self.decode(token)
+    body = JWT.decode(token, SECRET_KEY)[0]
+
+    # It enables to access hash properties using both a symbol Hash[:prop] and a Hash['prop']
+    HashWithIndifferentAccess.new body
+  end
+
+  rescue JWT::DecodeError => e
+    raise ExceptionHandler::InvalidToken, e.message
+  end
+end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,9 @@
-# Generate the initial User model test data 
+# Generate the initial User model test data
 FactoryBot.define do
   factory :user do
     first_name { Faker::Name.name }
     last_name { Faker::Name.name }
-    email {'foo@bar.com'}
-    password {'foobar'}
+    email { 'foo@bar.com' }
+    password { 'foobar' }
   end
 end
