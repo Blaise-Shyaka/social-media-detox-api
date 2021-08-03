61745ac7d53b5144caae48c75566ea637e220730
Blaise-Shyaka <blaiseshyaka95@gmail.com>
2021-08-04 00:46:51 +0200

    enable CORS

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,25 @@
 class ApplicationController < ActionController::API
   include ExceptionHandler
 
-  before_action :authorize_request
+  before_action :authorize_request, :cors_preflight_check
+  after_action :cors_set_access_control_headers
   attr_reader :current_user
+
+  def cors_preflight_check
+    if request.method == :options
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+      headers['Access-Control-Allow-Headers'] = '*'
+      headers['Access-Control-Request-Method'] = '*'
+    end
+  end
+
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = '*'
+    headers['Access-Control-Request-Method'] = '*'
+  end
 
   private
 
